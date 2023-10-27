e3a7e21de073e43ac8bd4e924d8bea6ea777e5af
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-10-26 20:07:20 -0600

    Add ship class and tests

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,1 +1,34 @@
 require 'spec_helper'
+
+require "./lib/ship.rb"
+
+RSpec.describe Ship do
+  it "exists and has a name and length and health" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser).to be_a Ship
+    expect(cruiser.name).to eq("Cruiser")
+    expect(cruiser.length).to eq(3)
+    expect(cruiser.health).to eq(3)
+  end
+
+  it "has a sunk? method" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.sunk?).to eq(false)
+  end
+
+  it "has a hit method" do
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser.hit
+    expect(cruiser.health).to eq(2)
+    cruiser.hit
+    expect(cruiser.sunk?).to eq(false)
+    expect(cruiser.health).to eq(1)
+    cruiser.hit
+    expect(cruiser.health).to eq(0)
+    expect(cruiser.sunk?).to be (true)
+  end
+
+  
+
+
+end
